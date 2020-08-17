d05ed2582889594089105170f160ab032345bbd3
Maidul-Islam-Shawon <mi_shawon@yahoo.com>
2020-08-18 04:12:49 +0600

    Updates

---
+++ precache-manifest.e8cdea7b4171df45330fd254417d5873.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9f09e3f6f3b0a38edaa73b04f0ff06",
+    "url": "/jmbp-web-dev-page/index.html"
+  },
+  {
+    "revision": "b15a427ad01982b9362a",
+    "url": "/jmbp-web-dev-page/static/css/2.eef52be1.chunk.css"
+  },
+  {
+    "revision": "e41778591f90dfb11141",
+    "url": "/jmbp-web-dev-page/static/css/main.87a0da45.chunk.css"
+  },
+  {
+    "revision": "b15a427ad01982b9362a",
+    "url": "/jmbp-web-dev-page/static/js/2.64e6b3df.chunk.js"
+  },
+  {
+    "revision": "b8fa2dc5aa7f3d680fd19a256eb70b35",
+    "url": "/jmbp-web-dev-page/static/js/2.64e6b3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e41778591f90dfb11141",
+    "url": "/jmbp-web-dev-page/static/js/main.4fa0fad9.chunk.js"
+  },
+  {
+    "revision": "a9cba2b44bb5a026e2e9",
+    "url": "/jmbp-web-dev-page/static/js/runtime-main.a84af64c.js"
+  },
+  {
+    "revision": "06ea1c1bf42f33e4655e0fc2811e06ea",
+    "url": "/jmbp-web-dev-page/static/media/AspNet.06ea1c1b.png"
+  },
+  {
+    "revision": "7d3c053858815c86ef3db1b32e73dd98",
+    "url": "/jmbp-web-dev-page/static/media/BespokeDesign.7d3c0538.png"
+  },
+  {
+    "revision": "4ce814e2351d681bb43a719fb60fb849",
+    "url": "/jmbp-web-dev-page/static/media/Bootstrap.4ce814e2.png"
+  },
+  {
+    "revision": "85e1a963386bad9951c1dde67ad8747e",
+    "url": "/jmbp-web-dev-page/static/media/Drupal.85e1a963.png"
+  },
+  {
+    "revision": "acad33e1fd40f078304b528e3eb9e2f7",
+    "url": "/jmbp-web-dev-page/static/media/E_Commerce.acad33e1.png"
+  },
+  {
+    "revision": "e17f7810d7e149749239adc5864da76e",
+    "url": "/jmbp-web-dev-page/static/media/Html.e17f7810.png"
+  },
+  {
+    "revision": "9df9e45d156de76b078671c6fdeb7b13",
+    "url": "/jmbp-web-dev-page/static/media/Javascript.9df9e45d.png"
+  },
+  {
+    "revision": "93794b6ac2f47f2d52ac5f3c01114e63",
+    "url": "/jmbp-web-dev-page/static/media/NextJs.93794b6a.png"
+  },
+  {
+    "revision": "899b3f885d617cb6f13af6bb4c9f91aa",
+    "url": "/jmbp-web-dev-page/static/media/React.899b3f88.png"
+  },
+  {
+    "revision": "f9a6124cc04c7609b17d994e53f96598",
+    "url": "/jmbp-web-dev-page/static/media/ResponsiveWeb.f9a6124c.png"
+  },
+  {
+    "revision": "182c3bfcf9057b5c1c0a39d122412f2f",
+    "url": "/jmbp-web-dev-page/static/media/SQLserver.182c3bfc.png"
+  },
+  {
+    "revision": "33889bec16002cc3dcde0359c153194f",
+    "url": "/jmbp-web-dev-page/static/media/VisualStudio.33889bec.png"
+  },
+  {
+    "revision": "aa9f569d0709ce106cc53ad49ef6fa24",
+    "url": "/jmbp-web-dev-page/static/media/Website Design.aa9f569d.png"
+  },
+  {
+    "revision": "9524bb690337a475088ece40fed3bc9c",
+    "url": "/jmbp-web-dev-page/static/media/about-jmbp-web-img-1.9524bb69.jpg"
+  },
+  {
+    "revision": "78f31a1df949c57ab8aab56a5821def0",
+    "url": "/jmbp-web-dev-page/static/media/about-jmbp-web-img-2.78f31a1d.jpg"
+  },
+  {
+    "revision": "4f1bb018848e759e25895e993f6e7d54",
+    "url": "/jmbp-web-dev-page/static/media/about-jmbp-web-img-3.4f1bb018.jpg"
+  },
+  {
+    "revision": "21d06f61a6ed2682881c3f0b58ee645d",
+    "url": "/jmbp-web-dev-page/static/media/c-sharp.21d06f61.png"
+  },
+  {
+    "revision": "9e1d23f65cee12a7762a84ce9a25b927",
+    "url": "/jmbp-web-dev-page/static/media/css.9e1d23f6.png"
+  },
+  {
+    "revision": "d81f89f322338195e8cff821e82bbd44",
+    "url": "/jmbp-web-dev-page/static/media/entityFramework.d81f89f3.png"
+  },
+  {
+    "revision": "2d93254962837fd9bbb3bbac9adb0d06",
+    "url": "/jmbp-web-dev-page/static/media/jmbp-footer-3_.2d932549.jpg"
+  },
+  {
+    "revision": "24867aa8cc158f377b64cd9c2edbc1d6",
+    "url": "/jmbp-web-dev-page/static/media/jmbp-premier-Img-1.24867aa8.jpg"
+  },
+  {
+    "revision": "ffe445195674e7a975e3409688f2fa39",
+    "url": "/jmbp-web-dev-page/static/media/jmbp-soft-img-1.ffe44519.jpg"
+  },
+  {
+    "revision": "1da4532e291f3cfdabd5c28cdb7c49c6",
+    "url": "/jmbp-web-dev-page/static/media/jmbp-soft-why-choose-us.1da4532e.jpg"
+  },
+  {
+    "revision": "da46d9c2a68830a5ca77ded1a8894e58",
+    "url": "/jmbp-web-dev-page/static/media/jmbp-web-premier-bg.da46d9c2.jpg"
+  },
+  {
+    "revision": "3969f0bdd6206e9b17e4f0cda27a2866",
+    "url": "/jmbp-web-dev-page/static/media/jmbp-web-solutions-Banner-2.3969f0bd.jpg"
+  },
+  {
+    "revision": "25f3c29806ecd745dacdd986ff5a457c",
+    "url": "/jmbp-web-dev-page/static/media/jmbp-web-solutions-Banner-3.25f3c298.jpg"
+  },
+  {
+    "revision": "3fcabaa5c49bbe1cb24c4aa3d7a6f9ef",
+    "url": "/jmbp-web-dev-page/static/media/jmbp-web-solutions-Banner-4.3fcabaa5.jpg"
+  },
+  {
+    "revision": "a433d56029444bd24a3ce4437b638049",
+    "url": "/jmbp-web-dev-page/static/media/jmbp-web-solutions-Banner-5.a433d560.jpg"
+  },
+  {
+    "revision": "ea98433427ac7df4b8937082a15501e7",
+    "url": "/jmbp-web-dev-page/static/media/jmbp-web-solutions-Banner-9.ea984334.jpg"
+  },
+  {
+    "revision": "32fa32782d608558b7dba15bce2b77cd",
+    "url": "/jmbp-web-dev-page/static/media/jmbp_Cms.32fa3278.jpg"
+  },
+  {
+    "revision": "f6737ec7f50963ff4005d46060ebb010",
+    "url": "/jmbp-web-dev-page/static/media/jmbp_Ecommerce.f6737ec7.jpg"
+  },
+  {
+    "revision": "637fb31b687bfbb7b0be63fc1dbb50f7",
+    "url": "/jmbp-web-dev-page/static/media/jmbp_Graphic_Design.637fb31b.jpg"
+  },
+  {
+    "revision": "815bf3e0e3e06a8dc32778bbbe3ce6f0",
+    "url": "/jmbp-web-dev-page/static/media/jmbp_Mobile_App.815bf3e0.jpg"
+  },
+  {
+    "revision": "d2d60bb547393e3dde88c184ca8effa8",
+    "url": "/jmbp-web-dev-page/static/media/jmbp_Seo.d2d60bb5.jpg"
+  },
+  {
+    "revision": "339c00dc26bab1141679f835ae73c97d",
+    "url": "/jmbp-web-dev-page/static/media/jmbp_SocialMedia.339c00dc.jpg"
+  },
+  {
+    "revision": "894eda9f0a2c82eb747ff6b811cdc2f9",
+    "url": "/jmbp-web-dev-page/static/media/jmbp_Web_Design.894eda9f.jpg"
+  },
+  {
+    "revision": "53004532ad2235fa15c3b92ce7ef541a",
+    "url": "/jmbp-web-dev-page/static/media/jmbp_Web_Development.53004532.jpg"
+  },
+  {
+    "revision": "37672c8b11a59f415346e560443b82ec",
+    "url": "/jmbp-web-dev-page/static/media/jmbp_Web_Hosting.37672c8b.jpg"
+  },
+  {
+    "revision": "e0036c051d475cf523d95078ac8cf9c8",
+    "url": "/jmbp-web-dev-page/static/media/jquery.e0036c05.png"
+  },
+  {
+    "revision": "1c093eb3287fd07dffddbc36efc1251c",
+    "url": "/jmbp-web-dev-page/static/media/magento.1c093eb3.png"
+  },
+  {
+    "revision": "76b604a04a60483a018a5009b45ba244",
+    "url": "/jmbp-web-dev-page/static/media/netCore.76b604a0.png"
+  },
+  {
+    "revision": "908e4ea1f73276348de4fd9f71dc3e18",
+    "url": "/jmbp-web-dev-page/static/media/nodejs.908e4ea1.png"
+  },
+  {
+    "revision": "33c00deb1452601e1d0ee4214eaa2e10",
+    "url": "/jmbp-web-dev-page/static/media/serviceBG.33c00deb.jpg"
+  },
+  {
+    "revision": "beb64408e7499969a3ef4fc16fbdc35b",
+    "url": "/jmbp-web-dev-page/static/media/serviceBanner-1.beb64408.jpg"
+  },
+  {
+    "revision": "4467614811db88cb19893059af589a88",
+    "url": "/jmbp-web-dev-page/static/media/wordpress.44676148.png"
+  }
+]);
